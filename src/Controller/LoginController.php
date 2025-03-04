7fdd3b7faa11922604d9f61694a32317f0f27a00
YoussefGri <yyoussef781@gmail.com>
2025-03-04 22:36:07 +0100

    login v1 ok

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+// src/Controller/LoginController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login', methods: ['POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): JsonResponse
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->json([
+            'last_username' => $lastUsername,
+            'error' => $error ? $error->getMessageKey() : null,
+        ]);
+    }
+}
